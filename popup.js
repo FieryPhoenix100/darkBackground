583cef27c4978bc63448fa1bd359cc3a7c48093c
FieryPhoenix <chunya.s10@gmail.com>
2020-01-12 13:13:37 +0300

    Added color picker

--- popup.js
+++ popup.js
@@ -25,4 +25,24 @@ window.onload = function () {
           });
         });
     }
+
+    gradient(document.getElementById("gradient"), 20, 180);
 }
+
+var gradient = (canva, w, h) => {
+    var context, gradient, hue;
+			 
+    context = canva.getContext("2d");
+
+    gradient = context.createLinearGradient(w/2,h,w/2,0);
+
+    hue = [[255,0,0],[255,255,0],[0,255,0],[0,255,255],[0,0,255],[255,0,255],[255,0,0]];
+				   
+	     for (var i=0; i <= 6;i++){
+             color = 'rgb('+hue[i][0]+','+hue[i][1]+','+hue[i][2]+')';
+              gradient.addColorStop(i*1/6, color);
+        };
+        
+        context.fillStyle = gradient;
+        context.fillRect(0,0, w ,h);	
+    }
